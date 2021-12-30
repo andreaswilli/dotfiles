6230c6f95b7ea83233f96f4050a01b8b7c5e93e9
Andreas Willi <andiwilli8460@gmail.com>
2021-12-30 20:17:43 +0100

    Install gnupg

--- run_once_before_homebrew.sh
+++ run_once_before_homebrew.sh
@@ -3,8 +3,10 @@
 # install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# TODO: install from Brewfile
 # install chezmoi to path (was temporarily installed in $TMPDIR by install script)
 brew install chezmoi
+brew install gnupg
 
 # install software with Homebrew/mas-cli
 # HOMEBREW_CASK_OPTS=--no-quarantine brew bundle
