1bd16eea1843db918761e5f9844fbaa457d98e2b
Andreas Willi <andiwilli8460@gmail.com>
2022-01-01 20:51:17 +0100

    Hide dock

--- run_once_after_03-macos-settings.sh
+++ run_once_after_03-macos-settings.sh
@@ -5,4 +5,10 @@ echo "⚙️  Changing macOS settings..."
 # trackpad
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
 
+# dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 1000
+defaults write com.apple.dock no-bouncing -bool TRUE
+killall Dock
+
 echo "⚙️  Done. Some settings get only applied after logout."
