93aa532dbd5980e8d8165c88ed52d6523c785b45
Andreas Willi <andiwilli8460@gmail.com>
2022-01-02 20:26:10 +0100

    Add application settings

---
+++ run_once_after_04-application-settings.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+echo "❇️  Applying application settings..."
+defaults write com.sindresorhus.Plash websites -array "'{\"usePrintStyles\":false,\"isCurrent\":true,\"invertColors2\":\"never\",\"id\":\"CABB3D95-79BD-4170-A55F-6A97CF135E39\",\"title\":\"Minecraft Weather\",\"css\":\"\",\"javaScript\":\"\",\"invertColors\":false,\"url\":\"https:\\/\\/andreaswilli.github.io\\/minecraft-weather?overlay=left\"}'"
+echo "❇️  Done."
