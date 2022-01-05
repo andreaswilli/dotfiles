4d573c4c7b5af51020f4c811db28e5424df82de4
Andreas Willi <andiwilli8460@gmail.com>
2022-01-05 20:14:35 +0100

    Format script

--- run_once_after_04-application-settings.sh
+++ run_once_after_04-application-settings.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
 echo "❇️  Applying application settings..."
+
 defaults write com.sindresorhus.Plash websites -array "'{\"usePrintStyles\":false,\"isCurrent\":true,\"invertColors2\":\"never\",\"id\":\"CABB3D95-79BD-4170-A55F-6A97CF135E39\",\"title\":\"Minecraft Weather\",\"css\":\"\",\"javaScript\":\"\",\"invertColors\":false,\"url\":\"https:\\/\\/andreaswilli.github.io\\/minecraft-weather?overlay=left\"}'"
+
 echo "❇️  Done."
