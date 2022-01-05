b0d30f31bdf80b08e07490d54cb1e036b2c801d8
Andreas Willi <andiwilli8460@gmail.com>
2022-01-05 20:40:04 +0100

    Do not install fonts if the exist already

--- run_once_after_07-fonts.sh
+++ run_once_after_07-fonts.sh
@@ -2,11 +2,24 @@
 
 echo "🔡 Installing fonts..."
 
-cd ~/Library/Fonts
+cd "$HOME/Library/Fonts"
+
+if [ ! -f "$HOME/Library/Fonts/MesloLGS NF Regular.ttf" ]; then
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf --output 'MesloLGS NF Regular.ttf'
+fi
+
+if [ ! -f "$HOME/Library/Fonts/MesloLGS NF Bold.ttf" ]; then
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf --output 'MesloLGS NF Bold.ttf'
+fi
+
+if [ ! -f "$HOME/Library/Fonts/MesloLGS NF Italic.ttf" ]; then
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf --output 'MesloLGS NF Italic.ttf'
+fi
+
+if [ ! -f "$HOME/Library/Fonts/MesloLGS NF Bold Italic.ttf" ]; then
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf --output 'MesloLGS NF Bold Italic.ttf'
+fi
+
 cd -
 
 echo "🔡 Done."
